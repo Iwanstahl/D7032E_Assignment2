290cc99cfa45ba7b48a50f48f5313c2b08f593ac
Linda El-Nagar <lineln-2@student.ltu.se>
2026-09-06 15:52:05 +0200

    added the last 500

--- assignment2testing-JAVA26/assignment3testing/src/ltu/PaymentTest.java
+++ assignment2testing-JAVA26/assignment3testing/src/ltu/PaymentTest.java
@@ -31,7 +31,7 @@ public class PaymentTest {
     public void testAge20GetsPayment() throws IOException {
         PaymentImpl payment = getPayment(2016, 1, 1);
         int result = payment.getMonthlyAmount("19960101-0000", 0, 100, 100);
-        assertTrue(result > 0);
+        assertEquals(9904, result);
     }
 
     // --- ID 102:The student may receive subsidiary until the year they turn 56 ---
@@ -45,7 +45,7 @@ public class PaymentTest {
     public void testAge56GetsSubsidy() throws IOException {
         PaymentImpl payment = getPayment(2016, 1, 1);
         int result = payment.getMonthlyAmount("19600101-0000", 0, 100, 100);
-        assertTrue(result > 0);
+        assertEquals(2816, result);
     }
 
     // --- ID 103: The student may not receive any student loans from the year they turn 47 --- 
@@ -132,18 +132,85 @@ public class PaymentTest {
         assertEquals(9904, result);
 
     }
-    // --- ID 501: Full time student loan: 7088 SEK / month ---                                                                                            
-                                                                                                                                                         
+    // --- ID 501: Full time student loan: 7088 SEK / month ---
+    @Test
+    public void testFullTimeLoanAmountIs7088() throws IOException {
+        PaymentImpl payment = getPayment(2016, 1, 1);
+        int loanPlusSubsidy = payment.getMonthlyAmount("19960101-0000", 0, 100, 100);
+        int subsidyOnly = payment.getMonthlyAmount("19690101-0000", 0, 100, 100);     
+        int loanAmount = loanPlusSubsidy - subsidyOnly;
+        assertEquals(7088, loanAmount);    
+    }                                                 
     // --- ID 502: Full time subsidiary: 2816 SEK / month ---
-
+    @Test
+    public void testFullTimeSubsidairyIs2816() throws IOException {
+        PaymentImpl payment = getPayment(2016, 1, 1);
+        int subsidyOnly = payment.getMonthlyAmount("19690101-0000", 0, 100, 100);     
+        assertEquals(2816, subsidyOnly);    
+    }
     // --- ID 503: Less than full time student loan: 3564 SEK / month ---
-
+    @Test
+    public void testLessThanFullTimeStudentLoanIs3564() throws IOException {
+        PaymentImpl payment = getPayment(2016, 1, 1);
+        int loanPlusSubsidy = payment.getMonthlyAmount("19960101-0000", 0, 99, 100);
+        int subsidyOnly = payment.getMonthlyAmount("19690101-0000", 0, 99, 100);     
+        int loanAmount = loanPlusSubsidy - subsidyOnly;
+        assertEquals(3564, loanAmount);    
+    }
     // --- ID 504: Less than full time subsidiary: 1396 SEK / month ---
-
+    @Test
+    public void testLessThanFullTimeSubsidairyIs1396() throws IOException {
+        PaymentImpl payment = getPayment(2016, 1, 1);
+        int subsidyOnly = payment.getMonthlyAmount("19690101-0000", 0, 99, 100);     
+        assertEquals(1396, subsidyOnly);    
+    }
     // --- ID 505: A person who is entitled to receive a student loan will always receive the full amount. ---
+    @Test
+    public void testLoanAmountNotEntitledWithIncome() throws IOException {
+    PaymentImpl payment = getPayment(2016, 1, 1);
+    int lowIncome = payment.getMonthlyAmount("19960101-0000", 0, 100, 100);
+    int nearLimitIncome = payment.getMonthlyAmount("19960101-0000", 85813, 100, 100);
+    assertEquals(lowIncome, nearLimitIncome);
+    }
+    @Test
+    public void testLoanAmountNotEntitledWithWithCompletionRatio() throws IOException {
+    PaymentImpl payment = getPayment(2016, 1, 1);
+    int lowCompletion = payment.getMonthlyAmount("19960101-0000", 0, 100, 50);
+    int fullCompletion = payment.getMonthlyAmount("19960101-0000", 0, 100, 100);
+    assertEquals(lowCompletion, fullCompletion);
+    }
 
     // --- ID 506: Student loans and subsidiary is paid on the last weekday (Monday to Friday) every month. ---
-
+    @Test
+    public void testPaymentDateJanuary2016() throws IOException {
+    PaymentImpl payment = getPayment(2016, 1, 15);
+    assertEquals("20160129", payment.getNextPaymentDay()); // Jan 31 is Sunday -> Fri Jan 29
+    }
+    @Test
+    public void testPaymentDateFebruary2016() throws IOException {
+    PaymentImpl payment = getPayment(2016, 2, 15);
+    assertEquals("20160229", payment.getNextPaymentDay()); // leap year, Feb 29 is Monday
+    }
+    @Test
+    public void testPaymentDateMarch2016() throws IOException {
+    PaymentImpl payment = getPayment(2016, 3, 15);
+    assertEquals("20160331", payment.getNextPaymentDay()); // March 31 is Thursday
+    }
+    @Test
+    public void testPaymentDateApril2016() throws IOException {
+    PaymentImpl payment = getPayment(2016, 4, 15);
+    assertEquals("20160429", payment.getNextPaymentDay()); // April 30 is Saturday -> Fri April 29
+    }
+    @Test
+    public void testPaymentDateMay2016() throws IOException {
+    PaymentImpl payment = getPayment(2016, 5, 15);
+    assertEquals("20160531", payment.getNextPaymentDay()); // May 31 is Tuesday
+    }
+    @Test
+    public void testPaymentDateJune2016() throws IOException {
+    PaymentImpl payment = getPayment(2016, 6, 15);
+    assertEquals("20160630", payment.getNextPaymentDay()); // June 30 is Thursday
+    }
 
 
 
